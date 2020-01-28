77f8fa55594367e5b677cd82a69397406f0e177d
Tulsi Guanipa <tulsiguanipa@gmail.com>
2020-01-28 12:58:53 -0300

    pipes personalizados

--- src/app/pipes/capitalizado.pipe.ts
+++ src/app/pipes/capitalizado.pipe.ts
@@ -6,15 +6,23 @@ import { Pipe, PipeTransform } from '@angular/core';
 
 export class CapitalizadoPipe implements PipeTransform {
     
-    transform(value: string): string {
+    transform(value: string, todas:boolean = true ): string {
 
         value = value.toLowerCase();
 
         let nombres = value.split(" ");
 
+        if (todas){
+            
+            for (const i in nombres ){
+            nombres[i] = nombres[i][0].toUpperCase() + nombres[i].substr(1);
+        }
+        
 
-        for (let i in nombres ){
-            nombres[i] = nombres[i][0].toUpperCase() + nombres[i].substr(1)
+
+            
+        }else{
+            nombres[0] = nombres[0][0].toUpperCase() +  nombres[0].substr(1);
         }
         
         return nombres.join(" ");
